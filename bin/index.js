09bcdb322d74d99d1b21c50eb215f7bd2437040a
Omar Elsayed <omar.alsayed@syscodeia.ae>
2022-04-30 21:45:08 +0200

    fix exec fun

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 /* eslint-disable global-require */
-const exec = require('child_process');
+const { exec } = require('child_process');
 
 function callback(error, stdout, stderr) {
   if (error) {
