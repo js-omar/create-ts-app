35c6ae719621f2f507b3bc25fb29814434a4eb71
Omar Elsayed <omar.alsayed@syscodeia.ae>
2022-06-25 21:59:27 +0200

    ci: :heavy_plus_sign: add semantic release to github workflow actions

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,5 @@ module.exports = {
   // collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  testPathIgnorePatterns: ['content'],
 };
--- test/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { isDevMode } from '../src/utils';
-
-describe('application', () => {
-  // Test Dev Mode
-  test('is not Dev Mode', () => {
-    expect(isDevMode).toBe(false);
-  });
-});
